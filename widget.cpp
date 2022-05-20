e4b821725e766fc749f607ddeb5567a89c3f8c56
Ismael Avila <ismael.guada234015@gmail.com>
2022-05-20 13:36:07 -0500

    Se agregaron variables

--- widget.cpp
+++ widget.cpp
@@ -18,26 +18,32 @@ Widget::~Widget()
 
 void Widget::on_btnResolver_clicked()
 {
+
+    int num1 = ui->inputNum1->text().toInt();
+    int num2 = ui->inputNum2->text().toInt();
+
+    int resultado;
+
     switch(ui->comboOpe->currentIndex()) {
 
     case 0:
-        ui->labelResultado->setText(QString::number(ui->inputNum1->text().toInt() + ui->inputNum2->text().toInt()));
+        ui->labelResultado->setText("Resultado = " + QString::number(num1 + num2));
         break;
 
     case 1:
-        ui->labelResultado->setText(QString::number(ui->inputNum1->text().toInt() - ui->inputNum2->text().toInt()));
+        ui->labelResultado->setText("Resultado = " + QString::number(num1 - num2));
         break;
 
     case 2:
-        ui->labelResultado->setText(QString::number(ui->inputNum1->text().toInt() * ui->inputNum2->text().toInt()));
+        ui->labelResultado->setText("Resultado = " + QString::number(num1 * num2));
         break;
 
     case 3:
-        ui->labelResultado->setText(QString::number(ui->inputNum1->text().toInt() / ui->inputNum2->text().toInt()));
+        ui->labelResultado->setText("Resultado = " + QString::number(num1 / num2));
         break;
 
     case 4:
-        ui->labelResultado->setText(QString::number(ui->inputNum1->text().toInt() ^ ui->inputNum2->text().toInt()));
+        ui->labelResultado->setText("Resultado = " + QString::number(num1 ^ num2));
         break;
     }
 
