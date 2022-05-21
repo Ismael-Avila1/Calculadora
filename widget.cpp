9619ff959d4bbb4f661316814d0089d4189f7202
Ismael Avila <ismael.guada234015@gmail.com>
2022-05-20 23:35:04 -0500

    Se definió la estructura para implementar los calculos en ensamblador

--- widget.cpp
+++ widget.cpp
@@ -1,5 +1,8 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include <iostream>
+
+using namespace std;
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -16,37 +19,85 @@ Widget::~Widget()
 }
 
 
+int suma(int n1, int n2);
+int resta(int n1, int n2);
+int multiplicacion(int n1, int n2);
+int division(int n1, int n2);
+int potencia(int n1, int n2);
+
 void Widget::on_btnResolver_clicked()
 {
 
     int num1 = ui->inputNum1->text().toInt();
     int num2 = ui->inputNum2->text().toInt();
 
-    int resultado;
-
     switch(ui->comboOpe->currentIndex()) {
 
     case 0:
-        ui->labelResultado->setText("Resultado = " + QString::number(num1 + num2));
+        ui->labelResultado->setText("Resultado = " + QString::number(suma(num1, num2)));
         break;
 
     case 1:
-        ui->labelResultado->setText("Resultado = " + QString::number(num1 - num2));
+        ui->labelResultado->setText("Resultado = " + QString::number(resta(num1, num2)));
         break;
 
     case 2:
-        ui->labelResultado->setText("Resultado = " + QString::number(num1 * num2));
+        ui->labelResultado->setText("Resultado = " + QString::number(multiplicacion(num1, num2)));
         break;
 
     case 3:
-        ui->labelResultado->setText("Resultado = " + QString::number(num1 / num2));
+        ui->labelResultado->setText("Resultado = " + QString::number(division(num1, num2)));
         break;
 
     case 4:
-        ui->labelResultado->setText("Resultado = " + QString::number(num1 ^ num2));
+        ui->labelResultado->setText("Resultado = " + QString::number(potencia(num1, num2)));
         break;
     }
 
+}
 
+
+int suma(int n1, int n2) {
+    int resultado;
+
+    // __asm__();
+
+    return resultado;
+}
+
+
+int resta(int n1, int n2) {
+    int resultado;
+
+    // __asm__();
+
+    return resultado;
+}
+
+
+int multiplicacion(int n1, int n2) {
+    int resultado;
+
+    // __asm__();
+
+    return resultado;
+}
+
+
+int division(int n1, int n2) {
+    int resultado;
+
+    // __asm__();
+
+    return resultado;
+}
+
+
+int potencia(int n1, int n2) {
+    int resultado;
+
+    // __asm__();
+
+    return resultado;
 }
 
