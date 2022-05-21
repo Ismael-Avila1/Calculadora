74da14b77095cf02858f66c0e3207a8a993c664c
Ismael Avila <ismael.guada234015@gmail.com>
2022-05-21 12:45:40 -0500

    Se agregaron las operaciones de Seno y Coseno

--- widget.cpp
+++ widget.cpp
@@ -27,61 +27,101 @@ void Widget::on_btnResolver_clicked()
     float resultado;
 
     switch(ui->comboOpe->currentIndex()) {
+        case 0:
+            __asm__(
+                "FLD %1;"
+                "FLD %2;"
+                "FADDP;"
+                "FSTP %0;" : "=m"(resultado) : "g"(num1), "g"(num2)
+            );
 
-    case 0:
-        __asm__(
-            "FLD %1;"
-            "FLD %2;"
-            "FADDP;"
-            "FSTP %0;" : "=m"(resultado) : "g"(num1), "g"(num2)
-        );
+            ui->labelResultado->setText("Resultado = " + QString::number(resultado));
+            break;
 
-        ui->labelResultado->setText("Resultado = " + QString::number(resultado));
-        break;
 
-    case 1:
-        __asm__(
-            "FLD %1;"
-            "FLD %2;"
-            "FSUBP;"
-            "FSTP %0;" : "=m"(resultado) : "g"(num2), "g"(num1)
-        );
+        case 1:
+            __asm__(
+                "FLD %1;"
+                "FLD %2;"
+                "FSUBP;"
+                "FSTP %0;" : "=m"(resultado) : "g"(num2), "g"(num1)
+            );
 
-        ui->labelResultado->setText("Resultado = " + QString::number(resultado));
-        break;
+            ui->labelResultado->setText("Resultado = " + QString::number(resultado));
+            break;
 
-    case 2:
-        __asm__(
-            "FLD %1;"
-            "FLD %2;"
-            "FMULP;"
-            "FSTP %0;" : "=m"(resultado) : "g"(num1), "g"(num2)
-        );
 
-        ui->labelResultado->setText("Resultado = " + QString::number(resultado));
-        break;
+        case 2:
+            __asm__(
+                "FLD %1;"
+                "FLD %2;"
+                "FMULP;"
+                "FSTP %0;" : "=m"(resultado) : "g"(num1), "g"(num2)
+            );
 
-    case 3:
-        __asm__(
-            "FLD %1;"
-            "FLD %2;"
-            "FDIVP;"
-            "FSTP %0;" : "=m"(resultado) : "g"(num2), "g"(num1)
-        );
+            ui->labelResultado->setText("Resultado = " + QString::number(resultado));
+            break;
 
-        ui->labelResultado->setText("Resultado = " + QString::number(resultado));
-        break;
 
-    case 4:
-        __asm__(
-            "FLD %1;"
-            "FLD %2;"
-            "FADDP;"
-            "FSTP %0;" : "=m"(resultado) : "g"(num1), "g"(num2)
-        );
+        case 3:
+            __asm__(
+                "FLD %1;"
+                "FLD %2;"
+                "FDIVP;"
+                "FSTP %0;" : "=m"(resultado) : "g"(num2), "g"(num1)
+            );
 
-        ui->labelResultado->setText("Resultado = " + QString::number(resultado));
-        break;
+            ui->labelResultado->setText("Resultado = " + QString::number(resultado));
+            break;
+
+
+        case 4:
+            __asm__ __volatile__(
+                "FLD %1;"
+                "FSQRT;"
+                "FSTP %0;" : "=g"(resultado) : "g"(num1)
+            );
+
+            ui->labelResultado->setText("Resultado = " + QString::number(resultado));
+            ui->inputNum2->setText("");
+            break;
+
+
+        case 5: {
+            float dosAngulo = 180.0f;
+
+            __asm__ __volatile__(   "FLD %1;"
+                                    "FLD %2;"
+                                    "FLDPI;"
+                                    "FMULP;"
+                                    "FDIVP;"
+                                    "FSIN;"
+                                    "FSTP %0;" : "=m"(resultado) : "g"(dosAngulo), "g"(num1)
+                                    );
+
+            ui->labelResultado->setText("Resultado = " + QString::number(resultado));
+            ui->inputNum2->setText("");
+            break;
+        }
+
+
+        case 6:{
+            float dosAngulo = 180.0f, radianes;
+
+            radianes = num1 * 3.141592 / dosAngulo;
+
+            __asm__ __volatile__(   "FLD %1;"
+                                    "FCOS;"
+                                    "FSTP %0;" : "=m"(resultado) : "m"(radianes)
+            );
+
+            if(resultado < 0.000001)
+                resultado = 0;
+
+            ui->labelResultado->setText("Resultado = " + QString::number(resultado));
+            ui->inputNum2->setText("");
+            break;
+        }
     }
 
 }
